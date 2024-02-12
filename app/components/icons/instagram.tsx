8e3d64fd7b6fff2335ec6327e40924cf921e619e
Solmaz-Salehzadeh <solmaz.salehzadeh1369@gmail.com>
2024-02-12 10:27:12 +0330

    add footer ui

---
+++ app/components/icons/instagram.tsx
@@ -0,0 +1,12 @@
+const LocationIcon = () => {
+    return (
+<svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.74984 23.8337H16.2498C21.6665 23.8337 23.8332 21.667 23.8332 16.2503V9.75032C23.8332 4.33366 21.6665 2.16699 16.2498 2.16699H9.74984C4.33317 2.16699 2.1665 4.33366 2.1665 9.75032V16.2503C2.1665 21.667 4.33317 23.8337 9.74984 23.8337Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M13.0002 16.7913C15.0942 16.7913 16.7918 15.0938 16.7918 12.9997C16.7918 10.9056 15.0942 9.20801 13.0002 9.20801C10.9061 9.20801 9.2085 10.9056 9.2085 12.9997C9.2085 15.0938 10.9061 16.7913 13.0002 16.7913Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M19.1056 7.58366H19.1181" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+    )
+}
+
+export default LocationIcon
