0889e847a8a0bfccd96f2797e42334a94c9e2a12
amirhoseinshagholani <sh.amirhosein@gmail.com>
2024-03-24 13:56:50 +0330

    creating submit comment

--- app/(main)/submitcomment/page.tsx
+++ app/(main)/submitcomment/page.tsx
@@ -24,17 +24,67 @@ const Submitcomment = () => {
           </div>
         </div>
         <div className="grid grid-cols-7 mt-7">
-          <div className="col-span-2 border border-2 rounded rounded-3xl p-5">
-            <div className="text-xs grid grid-cols-2">
+          <div className="col-span-2 border border-2 rounded rounded-3xl p-6">
+            <div className="text-xs grid grid-cols-2 mb-9">
               <div className="col-span-1">قیمت و ارزش خرید</div>
-              <div className="col-span-1 justify-end flex" dir="ltr">
+              <div className="col-span-1" dir="ltr">
+                <form>
+                  <input type="range" min="1" max="5" step="1" />
+                </form>
+              </div>
+            </div>
+            <div className="text-xs grid grid-cols-2 mb-9">
+              <div className="col-span-1">کیفیت محصول</div>
+              <div className="col-span-1" dir="ltr">
+                <form>
+                  <input type="range" min="1" max="5" step="1" />
+                </form>
+              </div>
+            </div>
+            <div className="text-xs grid grid-cols-2 mb-9">
+              <div className="col-span-1">متریال</div>
+              <div className="col-span-1" dir="ltr">
+                <form>
+                  <input type="range" min="1" max="5" step="1" />
+                </form>
+              </div>
+            </div>
+            <div className="text-xs grid grid-cols-2 mb-9">
+              <div className="col-span-1">شباهت یا مغایرت</div>
+              <div className="col-span-1" dir="ltr">
+                <form>
+                  <input type="range" min="1" max="5" step="1" />
+                </form>
+              </div>
+            </div>
+            <div className="text-xs grid grid-cols-2 mb-3">
+              <div className="col-span-1">عمر مفید</div>
+              <div className="col-span-1" dir="ltr">
                 <form>
                   <input type="range" min="1" max="5" step="1" />
                 </form>
               </div>
             </div>
           </div>
-          <div className="col-span-5"></div>
+            <div className="col-span-5">
+              <form>
+                <div className="grid grid-cols-2 pr-5">
+                  <div className="col-span-1 text-sm">آرمان باقری | 22 بهمن 1402</div>
+                  <div className="col-span-1 justify-end flex mb-5">
+                      <div className="flex gap-2 justify-end">
+                        <input type="checkbox" />
+                        <div className="text-xs text-slate-500">دیدگاهم را به صورت ناشناس منتشر کن</div>
+                      </div>
+                  </div>
+                  <div className="col-span-2">
+                    <textarea className="bg-customGray w-full text-xs p-4 rounded rounded-3xl" placeholder="دیدگاه خود را بنویسید" rows="12"></textarea>
+                  </div>
+                  <div className="col-span-2 justify-end flex">
+                    <button className="border border-primary text-primary w-40 rounded rounded-2xl pt-3 pb-3 pr-7 pl-7">ثبت نظر</button>
+                  </div>
+                </div>
+              </form>
+            </div>
         </div>
       </div>
     </>
