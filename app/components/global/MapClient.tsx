65ad6dc1aff0ce9e28917ccd20b5540c4fb583bd
Arman Bagheri <armanbagherymosleh@gmail.com>
2025-02-01 22:40:16 +0330

    Fix map issue and refactor to new mapboxgl

--- app/components/global/MapClient.tsx
+++ app/components/global/MapClient.tsx
@@ -1,12 +1,9 @@
-import React, { memo, useEffect, useState, useRef } from "react";
-import { Map, Overlay, Feature } from "ol";
-import { Point } from "ol/geom";
-import { fromLonLat, toLonLat } from "ol/proj";
-import NeshanMap, {
-  NeshanMapRef,
-  Ol,
-  OlMap,
-} from "@neshan-maps-platform/react-openlayers";
+"use client";
+
+import { useEffect, useRef, useState, useCallback, memo } from "react";
+import "@neshan-maps-platform/mapbox-gl-react/dist/style.css";
+import mapboxgl from "@neshan-maps-platform/mapbox-gl";
+import Image from "next/image";
 
 const DEFAULT_LOCATION = {
   lat: 35.65326,
@@ -26,98 +23,129 @@ export interface MapProps {
   isAddressManuallyChanged?: boolean;
 }
 
-const Maps: React.FunctionComponent<MapProps> = ({
+const Maps: React.FC<MapProps> = ({
   defaultLocation = DEFAULT_LOCATION,
   onAddressChange,
   onLocationChange,
   isAddressManuallyChanged = false,
   height = 200,
 }) => {
-  const mapRef = useRef<NeshanMapRef | null>(null);
-  const [ol, setOl] = useState<Ol>();
-  const [olMap, setOlMap] = useState<OlMap>();
-  const [marker, setMarker] = useState<Overlay | null>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
+  const markerRef = useRef<HTMLDivElement>(null); // Ref for the marker div
   const [currentLocation, setCurrentLocation] = useState<LocationState>({
     lat: null,
     lng: null,
   });
   const [lastLocation, setLastLocation] = useState<LocationState | null>(null);
-  const [hasUserInteracted, setHasUserInteracted] = useState(false);
-
-  const onInit = (ol: Ol, map: OlMap) => {
-    setOl(ol);
-    setOlMap(map);
-
-    const markerElement = document.createElement("div");
-    markerElement.className = "marker";
-    markerElement.style.width = "114px";
-    markerElement.style.height = "114px";
-    markerElement.style.backgroundImage = "url('/assets/map/img_pin.png')";
-    markerElement.style.backgroundSize = "contain";
-
-    const markerFeature = new Feature({
-      geometry: new Point(
-        fromLonLat([defaultLocation.lng, defaultLocation.lat])
-      ),
-    });
-
-    const markerOverlay = new Overlay({
-      element: markerElement,
-      positioning: "bottom-center",
-      stopEvent: false,
-      feature: markerFeature,
-    });
-
-    map.addOverlay(markerOverlay);
-    setMarker(markerOverlay);
-
-    setTimeout(() => {
-      const view = map.getView();
-      view.animate({
-        center: fromLonLat([defaultLocation.lng, defaultLocation.lat]),
-        zoom: 12,
-        duration: 1000,
-      });
-    }, 2000);
-  };
 
   useEffect(() => {
-    if (olMap && marker) {
-      olMap.on("moveend", () => {
-        const center = olMap.getView().getCenter();
-        if (center) {
-          const [longitude, latitude] = toLonLat(center);
-          const newLocation = { lat: latitude, lng: longitude };
+    if (mapContainerRef.current) {
+      mapRef.current = new mapboxgl.Map({
+        mapType: mapboxgl.Map.mapTypes.neshanVector,
+        container: mapContainerRef.current,
+        zoom: 12,
+        pitch: 0,
+        center: [defaultLocation.lng, defaultLocation.lat],
+        minZoom: 2,
+        maxZoom: 21,
+        trackResize: true,
+        mapKey: "web.17a0c7f735234ae7acfa9eac73c9ca2e",
+        poi: false,
+        traffic: false,
+        mapTypeControllerOptions: {
+          show: false,
+        },
+      }) as unknown as mapboxgl.Map;
+    }
+
+    return () => mapRef.current?.remove();
+  }, []);
+
+  useEffect(() => {
+    const neshanMap = mapRef.current;
+    if (neshanMap) {
+      neshanMap.on("moveend", () => {
+        if (neshanMap) {
+          const center = neshanMap.getCenter();
+          const newLocation = { lat: center.lat, lng: center.lng };
 
           if (
             !lastLocation ||
             lastLocation.lat !== newLocation.lat ||
             lastLocation.lng !== newLocation.lng
           ) {
-            onLocationChange?.(newLocation);
+            console.log("Location changed:", newLocation);
             setCurrentLocation(newLocation);
             setLastLocation(newLocation);
-            setHasUserInteracted(true); // User has interacted with the map
+            onLocationChange?.(newLocation);
           }
         }
       });
     }
-  }, [olMap, marker, ol, onLocationChange, lastLocation]);
+  }, []); // Empty dependency array, runs only once after map is initialized
 
-  // Reset logic can be added here if needed, e.g., button click to reset map position
+  useEffect(() => {
+    if (
+      mapRef.current &&
+      currentLocation.lat &&
+      currentLocation.lng &&
+      (currentLocation.lat !== lastLocation?.lat ||
+        currentLocation.lng !== lastLocation?.lng)
+    ) {
+      mapRef.current.setCenter([currentLocation.lng, currentLocation.lat]);
+      if (markerRef.current) {
+        // Update marker position
+        markerRef.current.style.left = `${getPixelX(
+          currentLocation.lng,
+          currentLocation.lat
+        )}px`;
+        markerRef.current.style.top = `${getPixelY(
+          currentLocation.lng,
+          currentLocation.lat
+        )}px`;
+      }
+      if (!isAddressManuallyChanged) {
+        onAddressChange?.("Address will be updated here");
+      }
+    }
+  }, [
+    currentLocation,
+    isAddressManuallyChanged,
+    onAddressChange,
+    lastLocation,
+  ]);
+
+  const getPixelX = (lng, lat) => {
+    if (mapRef.current) {
+      const point = mapRef.current.project([lng, lat]);
+      return point.x;
+    }
+    return 0;
+  };
+
+  const getPixelY = (lng, lat) => {
+    if (mapRef.current) {
+      const point = mapRef.current.project([lng, lat]);
+      return point.y;
+    }
+    return 0;
+  };
 
   return (
-    <NeshanMap
-      mapKey="web.17a0c7f735234ae7acfa9eac73c9ca2e"
-      defaultType="neshan"
-      center={{
-        latitude: defaultLocation.lat ?? currentLocation.lat,
-        longitude: defaultLocation.lng ?? currentLocation.lng,
-      }}
-      style={{ height: `${height}px`, width: "100%" }}
-      onInit={onInit}
-      zoom={12}
-    />
+    <div
+      style={{ height: `${height}px`, width: "100%", position: "relative" }}
+      ref={mapContainerRef}
+    >
+      <div ref={markerRef} className="marker">
+        <Image
+          alt=""
+          width={114}
+          height={114}
+          src={"/assets/map/img_pin.png"}
+        />
+      </div>
+    </div>
   );
 };
 
